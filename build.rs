213a2f4129bb09a4f00c6e93a7bc9ce2099abe6b
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2023-03-15 23:00:05 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src//main.rs"); 
     // Use the `rustc` command to compile the Rust file to an executable.
-    std::process::Command::new("rustc")
-        .args(&["src//main.rs", "-o", "wifu.exe"]) 
+    std::process::Command::new("cargp")
+        .args(&["build", "--release", "wifu.exe"]) 
         .status()
         .unwrap();
 }
