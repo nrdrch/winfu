8a3840f8538f45c89c8ddc74696b3d32de1243f3
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2023-03-15 23:37:31 +0100

    Delete build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src/main.rs"); 
-    // Use the `cargo` command to build the crate with optimizations enabled.
-    std::process::Command::new("cargo")
-        .args(&["build", "--release"]) 
-        .status()
-        .unwrap();
-}
