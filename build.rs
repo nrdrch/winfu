a9830b1f8bf618a69c5f810937cd0c4b07b47b6b
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2023-03-15 23:14:52 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src//main.rs"); 
+    println!("cargo:rerun-if-changed=src/main.rs"); 
     // Use the `cargo` command to build the crate with optimizations enabled.
     std::process::Command::new("cargo")
         .args(&["build", "--release"]) 
