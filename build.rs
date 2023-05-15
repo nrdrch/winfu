5ff5053c8684a7afc28c914e481d47aef5fbd7cf
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2023-05-15 10:23:05 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,14 +1,29 @@
 use std::process::Command;
+
 fn main() {
+    // Check if the `bat` binary is installed
+    let bat_installed = Command::new("cargo")
+        .arg("install")
+        .arg("--list")
+        .output()
+        .expect("Failed to run `cargo install --list` command")
+        .stdout
+        .contains(&b"bat"[..]);
+
+    if !bat_installed {
+        // Run the `cargo install bat` command if `bat` is not installed
+        Command::new("cargo")
+            .arg("install")
+            .arg("bat")
+            .status()
+            .expect("Failed to run `cargo install bat` command");
+    }
+
+    // Build the main.rs file using `cargo build`
     Command::new("cargo")
         .current_dir("src")
         .arg("build")
         .arg("--release")
         .status()
         .expect("Failed to build main.rs using `cargo build`");
-    Command::new("cargo")
-        .arg("install")
-        .arg("bat")
-        .status()
-        .expect("Failed to run `cargo install bat` command");
 }
